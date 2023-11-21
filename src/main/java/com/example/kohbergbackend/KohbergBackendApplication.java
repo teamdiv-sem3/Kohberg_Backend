c428c2b5d904a3d9ae9e09e3e9a04ed3bfe310b9
Maloulou99 <113166624+Maloulou99@users.noreply.github.com>
2023-11-21 12:44:50 +0100

    test

--- src/main/java/com/example/kohbergbackend/KohbergBackendApplication.java
+++ src/main/java/com/example/kohbergbackend/KohbergBackendApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KohbergBackendApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(KohbergBackendApplication.class, args);
     }
